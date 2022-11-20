74a777ebd42b63e1a9a5084e6a0242ee47e485c2
neptuno7 <53880298+neptuno7@users.noreply.github.com>
2022-11-20 16:36:35 +0100

    Add files via upload

--- app/app/erregistratu.php
+++ app/app/erregistratu.php
@@ -6,13 +6,13 @@
   <body>
     <div class="sesion">
       <div class="buttons">
-        <input type="submit" onclick="itzuli()" value="Itzuli">
+        <input type="submit" onclick="window.location.href='index.html'" value="Itzuli">
       </div>
-      <script>
+      <!--<script>
         function itzuli(){
         window.location.href="index.html"
       }
-      </script>
+      </script>-->
     </div>
   </body>
 </html>
@@ -22,30 +22,39 @@
   $password = "test";
   $db = "database";
 
-  $izena=$_POST['Izena'];
-  $abizenak=$_POST['Abizenak'];
-  $NAN=$_POST['NAN'];
-  $telefonoa=$_POST['Telefonoa'];
-  $jaio=$_POST['Jaiotze_data'];
-  $email=$_POST['Email'];
-  $pasahitza=$_POST['Pasahitza'];
+
+  //$izena=$_POST['Izena'];
+  //$abizenak=$_POST['Abizenak'];
+  //$NAN=$_POST['NAN'];
+  //$telefonoa=$_POST['Telefonoa'];
+  //$jaio=$_POST['Jaiotze_data'];
+  //$email=$_POST['Email'];
+  //$pasahitza=$_POST['Pasahitza'];
   //$pasahitza=hash('sha512', $pasahitza); // pasahitza zifratu
 
   // konexioa ireki
   $conn = mysqli_connect($hostname,$username,$password,$db);
+  $izena = mysqli_real_escape_string($conn,$_POST['Izena']);
+  $abizenak = mysqli_real_escape_string($conn,$_POST['Abizenak']);
+  $NAN = mysqli_real_escape_string($conn,$_POST['NAN']);
+  $telefonoa = mysqli_real_escape_string($conn,$_POST['Telefonoa']);
+  $jaio = mysqli_real_escape_string($conn,$_POST['Jaiotze_data']);
+  $email = mysqli_real_escape_string($conn,$_POST['Email']);
+  $pasahitza = mysqli_real_escape_string($conn,$_POST['Pasahitza']);
   if ($conn->connect_error) {
     die("Database connection failed: " . $conn->connect_error);
   }
   else{
    // 'erabiltzaile' datu basean gehitu
-    $query = "INSERT INTO `erabiltzaileak`(`Izena`, `Abizenak`, `NAN`, `Telefonoa`, `Jaiotze_data`, `Email`, `Pasahitza`) 
-    VALUES ('$izena','$abizenak','$NAN','$telefonoa','$jaio','$email','$pasahitza')";
+   $query = $conn->prepare("INSERT INTO `erabiltzaileak`(`Izena`, `Abizenak`, `NAN`, `Telefonoa`, `Jaiotze_data`, `Email`, `Pasahitza`) 
+    VALUES ('$izena','$abizenak','$NAN','$telefonoa','$jaio','$email','$pasahitza')");
+   $query->execute();
   }
   $resultado = mysqli_query($conn,$query);
   if($resultado){
-    echo "<script> alert('Erregistroa sortu da, $izena!'); window.location='index.html' </script>";
+    echo "<script> alert('Erregistroa sortu da, $izena!'); window.location.href='index.html' </script>";
   } else {
-    echo "<script> alert('EZ da erregistroa sortu! Daturen bat okerra da.'); window.location='erregistroorria.html' </script>";
+    echo "<script>window.location.href='erregistroorria.html'</script>";
   }
   
   mysqli_close($conn);
--- app/app/login.php
+++ app/app/login.php
@@ -2,20 +2,29 @@
   session_start(); // saioa ireki
   $_SESSION["Izena"] = $izena;
   $_SESSION["Pasahitza"] = $pasahitza;
-  
-  // konexioa irekitzeko beharrezko datuak (datu basea, erabiltzaile, pasahitz eta datu basearen izena)
+  // konexioa irekitzeko beharrezko datuak (datu basea, erabiltzaile, pasahitz eta datu basearen  izena
   $hostname = "db";
   $username = "admin";
   $password = "test";
   $db = "database";
+  header('x-Frame-Options: SAMEORIGIN');
+  header('X-Content-Type-Options: nosniff');
+  header("Content-Security-Policy: default-src 'self'");
+  header_remove('X-Powered-By');
   
-  // izena eta pasahitza lortu
-  $izena=$_POST['Izena'];
-  $pasahitza=$_POST['Pasahitza'];
+  
+  // izena eta pasahitza lortu;
+  //$izena=$_POST['Izena'];
+  //$pasahitza=$_POST['Pasahitza'];
+  
   //$pasahitza=hash('sha512', $pasahitza); // pasahitza zifratu
 
   // konexioa ireki
+  
   $conn = mysqli_connect($hostname,$username,$password,$db);
+  $izena = mysqli_real_escape_string($conn,$_POST['Izena']);
+  $pasahitza = mysqli_real_escape_string($conn,$_POST['Pasahitza']);
+  
   if ($conn->connect_error) {
     die("Database connection failed: " . $conn->connect_error);
   }
@@ -23,14 +32,15 @@
     // 'erabiltzailea' konprobatu
     $sql = "SELECT * FROM `erabiltzaileak` WHERE  `Izena`='$izena' AND `Pasahitza`='$pasahitza'";
     $query = mysqli_query($conn,$sql);
-    // erabiltzairea baldin badago, nr ez da 0 izango
+  // erabiltzairea baldin badago, nr ez da 0 izango
     $nr = mysqli_num_rows($query);
   }
   if($nr>0){
     echo "<script> alert('Ongi etorri $izena'); window.location='datuAldatu.html' </script>";
   } else {
-    echo "<script> alert('Izena edo pasahitza okerra da, saiatu berriro! Edo erregistro berria sortu.'); window.location='index.html' </script>";
+    echo "<script> window.location.href='index.html' </script>";
   }
-
   mysqli_close($conn);
+
+  
 ?>
